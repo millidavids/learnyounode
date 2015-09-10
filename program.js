e306102e9e360d5d12c46a317ac6a8dea6a450a7
David Yurek <david.a.yurek@gmail.com>
2015-09-10 15:12:11 -0400

    async callback file read

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 
-var buf = fs.readFileSync(process.argv[2]);
-console.log(buf.toString().split("\n").length - 1);
+fs.readFile(process.argv[2], function callback(err, data){
+    console.log(data.toString().split("\n").length - 1);
+});
