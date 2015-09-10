5575caf027efc41436b31d6791374e73048562d4
David Yurek <david.a.yurek@gmail.com>
2015-09-10 14:38:34 -0400

    babysteps

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD');
+function addArgs(array) {
+    total = 0;
+    for(var i = 0; i < array.length; i++) {
+        total += +array[i];
+    }
+    return total;
+}
+
+console.log(addArgs(process.argv.slice(2, process.argv.length)));
