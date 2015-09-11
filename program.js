b3696c495b03069baa1c4d66be1946b12ef42796
David Yurek <david.a.yurek@gmail.com>
2015-09-11 17:22:03 -0400

    filtered ls

--- program.js
+++ program.js
@@ -1,5 +1,15 @@
 var fs = require('fs');
+var re = /.txt/;
 
-fs.readFile(process.argv[2], function callback(err, data){
-    console.log(data.toString().split("\n").length - 1);
+fs.readdir(process.argv[2], function (err, list){
+    filterFileType(list, process.argv[3]);
 });
+
+function filterFileType(list, type) {
+    var pattern = "[\\w-]+\\." + type;
+    for(var i = 0; i < list.length; i++){
+        if (list[i].match(new RegExp(pattern))){
+           console.log(list[i]);
+        }
+    }
+}
