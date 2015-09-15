6297ba27d398e0e34bd4fd31ed26ae26d90859a4
David Yurek <david.a.yurek@gmail.com>
2015-09-15 10:10:20 -0400

    http json api server

--- program.js
+++ program.js
@@ -1,7 +1,32 @@
-var http = require('http');
-var fs = require('fs');
+var http = require('http'),
+    map = require('through2-map'),
+    url = require('url'),
+    port = process.argv[2];
+
+var routes = {
+    "/api/parsetime": function(parsedUrl){
+        date = new Date(parsedUrl.query.iso);
+        return {
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds()
+        };
+    },
+    "/api/unixtime": function(parsedUrl){
+        return {unixtime: (new Date(parsedUrl.query.iso)).getTime()};
+    }
+};
+
 var server = http.createServer(function(req, res){
-    res.writeHead(200);
-    fs.createReadStream(process.argv[3]).pipe(res);
+    var parsedUrl = url.parse(req.url, true);
+    resource = routes[parsedUrl.pathname];
+    if (resource){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(resource(parsedUrl)));
+    }else{
+        res.writeHead(404);
+        res.end();
+    }
 });
-server.listen(process.argv[2]);
+
+server.listen(port);
